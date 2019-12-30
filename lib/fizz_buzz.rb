6b66762a8c148296d217cccd7db1becb69a33ff3
rutko <t.222cm@gmail.com>
2019-12-30 22:54:08 +0900

    ✨Add fizz_buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Fizz Buzz
+def fizz_buzz(n)
+	if n % 15 == 0
+		'FizzBuzz'
+	elsif n % 3 == 0
+		'Fizz'
+	elsif n % 5 == 0
+		'Buzz'
+	else
+	  n.to_s
+	end
+end
+
