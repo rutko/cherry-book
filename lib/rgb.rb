2da0729e97dd02aaef1a5f838d3bf954f84c1d5b
rutko <t.222cm@gmail.com>
2020-01-03 01:52:29 +0900

    ✨Add RGB

---
+++ lib/rgb.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# RGB
+def to_hex(r, g, b)
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  hex.scan(/\w\w/).map(&:hex)
+end
